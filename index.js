64785d70e18a4db8f0abef9c156ce68414e5893f
Nzix <iminezn5656@outlook.com>
2019-07-04 00:59:08 +0800

    fix ELECTRON_RUN_AS_NODE, support args

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ const puppeteer = require('puppeteer-core')
 
 let child
 
-const launch = (headless = true) =>
+const launch = options =>
 	new Promise(resolve => {
-		child = spawn(electron, [path.join(__dirname, 'main.js'), headless ? 'headless' : null])
+		let env = Object.assign({}, options.env || process.env)
+		if('ELECTRON_RUN_AS_NODE' in env) delete env.ELECTRON_RUN_AS_NODE
+		if(!'headless' in options) options.headless = true
+		child = spawn(electron, [path.join(__dirname, 'main.js'), '--remote-debugging-port=8315', JSON.stringify(options)], {env})
 		child.stdout.on('data', data => {
+			console.log(data.toString())
 			if(data.toString() === 'ready') resolve()
 		})
 	})
@@ -41,8 +45,8 @@ const patch = browser =>
 
 module.exports.launch = (options = {}) =>
 	Promise.resolve()
-	.then(() => launch(options.headless))
+	.then(() => launch(options))
 	// .then(() => endpoint())
 	// .then(url => puppeteer.connect({browserWSEndpoint: url}))
-	.then(() => puppeteer.connect({browserURL: 'http://localhost:8315'}))
+	.then(() => puppeteer.connect({browserURL: 'http://localhost:8315', slowMo: options.slowMo, defaultViewport: options.defaultViewport}))
 	.then(browser => patch(browser))
--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const electron = require('electron')
 
-electron.app.commandLine.appendSwitch('remote-debugging-port', '8315')
+const options = JSON.parse(process.argv.pop())
+Array.from(options.args || []).filter(line => !line.includes('remote-debugging-port')).forEach(line =>
+	electron.app.commandLine.appendSwitch.apply(null, line.replace(/^--/, '').split('='))
+)
+
 electron.app.once('ready', () => {
-	let window = new electron.BrowserWindow({show: false})
+	const window = new electron.BrowserWindow({show: false})
 	window.loadURL(`file://${__dirname}/index.html`)
 	window.once('ready-to-show', () => {
-		if(process.argv.pop() !== 'headless') window.show()
+		if(!options.headless) window.show()
 		process.stdout.write('ready')
 	})
 })
