2f2cce6703660c79e0be5868dfb8fd153d9fcaad
nondanee <iminezn5656@outlook.com>
2020-05-09 23:11:35 +0800

    more for demo

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
 const puppeteer = require('.')
 
 ;(async () => {
-	const app = await puppeteer.launch({args: ['--autoplay-policy=no-user-gesture-required']})
+	const app = await puppeteer.launch({ args: ['--autoplay-policy=no-user-gesture-required'] })
 	const pages = await app.pages()
 	const [page] = pages
-	await page.goto('https://files.catbox.moe/5955ob.m4a')
 
-	setTimeout(async () => await app.close(), 15000)
+	const close = async () => await app.close()
+	await page.exposeFunction('close', close)
+	const audioHandle = await page.evaluateHandle(src => (new Audio(src)), 'https://files.catbox.moe/5955ob.m4a')
+	await page.evaluate(audio => audio.onended = close, audioHandle)
+	await page.evaluate(audio => audio.play(), audioHandle)
 })()
