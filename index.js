3d74728dbb9c47659bdc5565d376a7d81eaadc97
nondanee <iminezn5656@outlook.com>
2020-05-09 22:54:46 +0800

    format code & optimize electron getPath #1

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const http = require('http')
 const path = require('path')
-const spawn = require('child_process').spawn
+const { spawn } = require('child_process')
 
 const electron = require('electron')
 const puppeteer = require('puppeteer-core')
 
-let child
+let child = null
+const electronPath = typeof (electron) === 'string' ? electron : electron.app.getPath('exe')
 
-const launch = options =>
-	new Promise(resolve => {
-		let env = Object.assign({}, options.env || process.env)
-		if('ELECTRON_RUN_AS_NODE' in env) delete env.ELECTRON_RUN_AS_NODE
-		if(!('headless' in options)) options.headless = true
-		child = spawn(electron, [path.join(__dirname, 'main.js'), '--remote-debugging-port=8315', JSON.stringify(options)], {env})
-		child.stdout.on('data', data => {
-			if(data.toString() === 'ready') resolve()
-		})
+const launch = options => {
+	options = options || {}
+	const env = Object.assign({}, options.env || process.env)
+	if ('ELECTRON_RUN_AS_NODE' in env) delete env.ELECTRON_RUN_AS_NODE
+	if (!('headless' in options)) options.headless = true
+	const args = [path.join(__dirname, 'main.js'), '--remote-debugging-port=8315', JSON.stringify(options)]
+	return new Promise(resolve => {
+		const listener = data => data.toString() === 'ready' && resolve(child.stdout.off('data', listener))
+		child = spawn(electronPath, args, { env })
+		child.stdout.on('data', listener)
 	})
-
+}
+	
+/*
 const endpoint = () =>
 	Promise.resolve()
 	.then(() => new Promise((resolve, reject) => {
@@ -27,25 +31,32 @@ const endpoint = () =>
 		.end()
 	}))
 	.then(response => new Promise((resolve, reject) => {
-		let chunks = []
+		const chunks = []
 		response
 		.on('data', chunk => chunks.push(chunk))
-		.on('end', () => resolve(Buffer.concat(chunks).toString()))
+		.on('end', () => resolve(Buffer.concat(chunks)))
 		.on('error', error => reject(error))
 	}))
 	.then(body => JSON.parse(body))
 	.then(data => data.webSocketDebuggerUrl)
+*/
 
 const patch = browser =>
 	new Proxy(browser, {
-		get: (target, key, receiver) => key !== 'close' ? Reflect.get(target, key, receiver) : () => target.close().then(() => child.kill()),
-		set: (target, key, value, receiver) => Reflect.set(target, key, value, receiver)
+		get: (target, key, receiver) =>
+			key === 'close'
+				? () => target.close().then(() => child && child.kill())
+				: Reflect.get(target, key, receiver),
+		set: (target, key, value, receiver) =>
+			Reflect.set(target, key, value, receiver)
 	})
 
-module.exports.launch = (options = {}) =>
-	Promise.resolve()
+module.exports.launch = options => {
+	const { slowMo, defaultViewport } = options || {}
+	return Promise.resolve()
 	.then(() => launch(options))
 	// .then(() => endpoint())
-	// .then(url => puppeteer.connect({browserWSEndpoint: url}))
-	.then(() => puppeteer.connect({browserURL: 'http://localhost:8315', slowMo: options.slowMo, defaultViewport: options.defaultViewport}))
+	// .then(browserWSEndpoint => puppeteer.connect({ browserWSEndpoint, slowMo, defaultViewport }))
+	.then(() => puppeteer.connect({ browserURL: 'http://localhost:8315', slowMo, defaultViewport }))
 	.then(browser => patch(browser))
+}
