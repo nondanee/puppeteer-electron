8e6c5bdfe3beea66012dcbcd4078a54c9b327905
nondanee <iminezn5656@outlook.com>
2020-05-09 22:37:04 +0800

    shrink electron startup script

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-const electron = require('electron')
+const { app, BrowserWindow } = require('electron')
 
-const options = JSON.parse(process.argv.pop())
-Array.from(options.args || []).filter(line => !line.includes('remote-debugging-port')).forEach(line =>
-	electron.app.commandLine.appendSwitch.apply(null, line.replace(/^--/, '').split('='))
+let options = {}
+try { options = JSON.parse(process.argv.pop()) } catch (error) {}
+
+Array.from(options.args || [])
+.filter(line => !line.includes('remote-debugging-port'))
+.forEach(line =>
+	app.commandLine.appendSwitch.apply(null, line.replace(/^--/, '').split('='))
 )
 
-electron.app.once('ready', () => {
-	const window = new electron.BrowserWindow({show: false})
-	window.loadURL(`file://${__dirname}/index.html`)
+app.once('ready', () => {
+	const window = new BrowserWindow({ show: false })
+	window.loadURL('data:text/html')
 	window.once('ready-to-show', () => {
-		if(!options.headless) window.show()
+		if (!options.headless) window.show()
 		process.stdout.write('ready')
 	})
 })
